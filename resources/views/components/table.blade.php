115875f59ba5fa022ee2a78d63894016990cbef6
Noxo <noxoua@gmail.com>
2023-11-11 19:44:58 +0200

    redo table field

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -1,9 +1,12 @@
 @if (!empty($value))
+    @php
+        $fields = $field->table->getFields();
+    @endphp
     <x-filament-tables::table class="w-full overflow-hidden text-sm">
         <x-slot:header>
-            @foreach (array_keys($value[0]) as $key)
+            @foreach ($fields as $field)
                 <x-filament-tables::header-cell class="!py-2">
-                    {{ $key }}
+                    {{ $field->getLabel() }}
                 </x-filament-tables::header-cell>
             @endforeach
         </x-slot:header>
@@ -11,9 +14,9 @@
 
         @foreach ($value as $item)
             <x-filament-tables::row @class(['bg-gray-100/30 dark:bg-gray-900' => $loop->even])>
-                @foreach ($item as $_value)
+                @foreach ($fields as $field)
                     <x-filament-tables::cell class="px-4 py-2 align-top sm:first-of-type:ps-6 sm:last-of-type:pe-6">
-                        {{ $_value }}
+                        {{ $field->display(data_get($item, $field->name)) }}
                     </x-filament-tables::cell>
                 @endforeach
             </x-filament-tables::row>
--- src/ResourceLogger/Concerns/Types/Table.php
+++ src/ResourceLogger/Concerns/Types/Table.php
@@ -2,17 +2,31 @@
 
 namespace Noxo\FilamentActivityLog\ResourceLogger\Concerns\Types;
 
+use Noxo\FilamentActivityLog\ResourceLogger\Types\TableField;
+
 trait Table
 {
+    public ?TableField $table;
+
     public bool $tableDifferenceOnly = true;
 
-    public function table(bool $differenceOnly = true): static
+    public function table(array $fields = []): static
     {
         $this->type('table');
         $this->view('table');
-        $this->tableDifferenceOnly = $differenceOnly;
+        $this->table = TableField::make($fields);
 
         $this->formatStateUsing('array');
+        $this->resolveStateUsing(function ($record) {
+            $records = collect(data_get($record, $this->name));
+            $fields = collect($this->table->getFields());
+
+            return $records->map(function ($record) use ($fields) {
+                return $fields->mapWithKeys(fn ($field) => [
+                    $field->name => $field->getStorableValue($record),
+                ])->toArray();
+            })->toArray();
+        });
 
         return $this;
     }
